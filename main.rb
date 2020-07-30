3d525a8a7322da28780bc0e636a4a821fdd0c6ab
Joseph Burke <joseph_burke@outlook.com>
2020-07-30 14:27:39 +0100

    Add speed multiplier to #type

--- main.rb
+++ main.rb
@@ -1,7 +1,15 @@
 module TypeTextSlowly
-    def type_slow
+    def type(speed='medium')
+        case speed
+        when 'slow'
+            speed_multiplier = 0.3
+        when 'medium'
+            speed_multiplier = 0.2
+        when 'quick'
+            speed_multiplier = 0.1
+        end
         arr = self.split('')
-        arr.each {|letter| print letter; sleep 0.2 * rand(0.25) }
+        arr.each {|letter| print letter; sleep speed_multiplier * rand(0.25) }
         print "\n"
     end
 end
@@ -10,4 +18,9 @@ class String
     include TypeTextSlowly
 end
 
-"Welcome Adventurer!".type_slow
+def wait(num_of_seconds)
+    sleep num_of_seconds
+end
+
+
+
