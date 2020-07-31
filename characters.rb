be4129fe9c7ef613b80afefd15eac033155c7e38
Joseph Burke <joseph_burke@outlook.com>
2020-07-31 21:08:22 +0100

    Add Protagonist class to characters.rb. Include Laughing module in Character class

--- characters.rb
+++ characters.rb
@@ -4,14 +4,18 @@ require_relative './utilities.rb'
 class Character
     attr_accessor :name, :age, :friendliness
     include Speaking
+    include Laughing
     def initialize(name, age, friendliness)
         @name = name
         @age = age
         @friendliness = friendliness
     end
-
 end
 
-joe = Character.new("Joe Burke", 23, 0.5)
+class Protagonist < Character
+    def initialize(name, age, friendliness)
+        super
+    end
+end
 
-# joe.greet
+
