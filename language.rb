59295ed5fa12956193d816556ed13d4538c736ac
Joseph Burke <joseph_burke@outlook.com>
2020-07-31 21:09:55 +0100

    Refactor #greets. List Laughter class instances

--- language.rb
+++ language.rb
@@ -1,5 +1,5 @@
 require_relative './utilities.rb'
-
+# __________SPEECH SUPERCLASS__________
 class Speech
     attr_accessor :words_spoken, :friendliness
     include Utilities
@@ -12,7 +12,7 @@ class Speech
         @words_spoken.type(speed)
     end
 end
-
+# __________GREETING SUBCLASS__________
 class Greeting < Speech
     def initialize(*args)
         super
@@ -20,31 +20,53 @@ class Greeting < Speech
     end
     POSSIBLE_GREETINGS_ARRAY = []
 end
+# __________LAUGHTER SUBCLASS__________
+class Laughter < Speech
+    def initialize(*args)
+        super
+        POSSIBLE_LAUGHS_ARRAY.push(self)
+    end
+    POSSIBLE_LAUGHS_ARRAY = []
+end
 
-
+# __________SPEAKING MODULE__________
 module Speaking
-    def greet
-        greeting_chosen = false
+    def greets
+        greeting = nil
         arr = Greeting::POSSIBLE_GREETINGS_ARRAY
-        until greeting_chosen 
+        while greeting.nil?
             possible_greeting = arr[rand(arr.length)]
-            friendliness_difference = (self.friendliness - possible_greeting.friendliness).round(1).abs
-            next if friendliness_difference > 0.2
-            case friendliness_difference
-            # 50% chance of saying that.
+            difference = (self.friendliness - possible_greeting.friendliness).round(1).abs
+            next if difference > 0.2
+            case difference
             when 0.0
-                (possible_greeting.say; greeting_chosen = true) if roll_dice(3)
-            # 33% chance of saying that.
+                (greeting = possible_greeting) if roll_dice(3)
             when 0.1
-                (possible_greeting.say; greeting_chosen = true) if roll_dice(2)
-            # 25% chance of saying that.
+                (greeting = possible_greeting) if roll_dice(2)
             when 0.2
-                (possible_greeting.say; greeting_chosen = true) if roll_dice(1)
+                (greeting = possible_greeting) if roll_dice(1)
             end
         end
+        says(greeting)
+    end
+
+    def says(speech_object, speed="medium")
+        empty_line
+        puts name + " says:"
+        empty_line
+        speech_object.words_spoken.type(speed)
+        empty_line
+        wait(1)
+    end
+end
+# __________LAUGHING MODULE__________
+module Laughing
+    def laughs
+        puts
     end
 end
 
+# __________GREETINGS INSTANCES__________
 Greeting.new("Greetings.", 0.4)
 Greeting.new("Hello.", 0.4)
 Greeting.new("How do you do?", 0.5)
@@ -58,5 +80,16 @@ Greeting.new("Hey!", 0.6)
 Greeting.new("How goes it?", 0.7)
 Greeting.new("What do you want?", 0.3)
 Greeting.new("What are you looking at?", 0.2)
+
+# __________LAUGHTER INSTANCES__________
+Laughter.new("What?", 0.1)
+Laughter.new("Har, har. Well aren't you funny...", 0.2)
+Laughter.new("Ha! That's a good one, I'll give you that.", 0.4)
+Laughter.new("He, he, he, nice one.", 0.5)
+Laughter.new("Hee-hee!", 0.6)
+Laughter.new("Ha, ha, ha. Very good.", 0.6)
+Laughter.new("Ha, ha, ha!", 0.6)
+Laughter.new("Ho, Ho, Ho!! Splendid, simply splendid!", 0.8)
+Laughter.new("Aaaaahahahaha!", 1)
 
 
