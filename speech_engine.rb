f6481bae31cbd192c4e1313620172d3014fb7097
Joseph Burke <joseph_burke@outlook.com>
2020-08-13 11:50:05 +0100

    Create basic prototype

---
+++ speech_engine.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+
+speech = 'Hello'
+
+
+
+
+
+class Person 
+    def initialize
+    end
+
+    def says(string)
+        puts string
+    end
+end
+
+speech = 'Hello'
+person = Person.new
+
+person.says(speech)
